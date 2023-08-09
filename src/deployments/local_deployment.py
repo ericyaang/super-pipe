8e2d385d308625575207892672352cfbb5c3a047
ericyaang <eric_yang@hotmail.com>
2023-08-09 09:47:03 -0300

    Local deployment tested

--- src/deployments/local_deployment.py
+++ src/deployments/local_deployment.py
@@ -1,23 +1,19 @@
-import os
-
 from prefect.deployments import Deployment
 from prefect.infrastructure import Process
-from prefect.infrastructure.container import DockerContainer
 from prefect.server.schemas.schedules import CronSchedule
-
 from flows.CornerShop import run_corner_shop_flow
-from core.cornershop.items import items_list as products_list
+
 
 deployment = Deployment.build_from_flow(
     flow=run_corner_shop_flow,
     name="Local-Deploy",
-    parameters={"items": products_list},
+    parameters={"items": ["haribo"]},
     version=1,
     tags=["ETL-Local"],
-    infrastructure=Process(working_dir=os.getcwd()),
-    # Run the flow at 03:00 pm every Monday
-    schedule=CronSchedule(cron="15 19 * * *", timezone="America/Sao_Paulo"),
+    infrastructure=Process(),
+    schedule=CronSchedule(cron="41 09 * * *", timezone="America/Sao_Paulo"),
     work_queue_name="default",
+    work_pool_name="default-agent-pool",
 )
 
 if __name__ == "__main__":
