cdd749e1031e3d96970f353fba2d621e96ab7893
ericyaang <eric_yang@hotmail.com>
2023-08-21 15:59:21 -0300

    removed .bash file

--- gcp_docker_build_commands.bash
+++
@@ -1,8 +0,0 @@
-docker build -t corner-etl/teste:latest .
-
-# to check the files in the container
-docker run --rm -it corner-etl/teste:latest
-
-docker tag corner-etl/teste:latest southamerica-east1-docker.pkg.dev/cornershop-390320/cornershop/corner-etl/teste:latest
-
-docker push southamerica-east1-docker.pkg.dev/cornershop-390320/cornershop/corner-etl/teste:latest
